83f563d4169b2bf054a0bcca2f0ac55f444bf2da
ChiefTwoPencils <robertwilk916@gmail.com>
2022-03-25 17:35:45 -0700

    Remove async.

--- api/Controllers/Ints.cs
+++ api/Controllers/Ints.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace PutPosts.Controllers
 {
@@ -7,15 +6,14 @@ namespace PutPosts.Controllers
     [ApiController]
     public class Ints : ControllerBase
     {
-        public async Task<IActionResult> Get()
-            => new JsonResult("it works");
+        public IActionResult Get() => new JsonResult("it works");
 
         [HttpPost]
-        public async Task<IActionResult> Post(List<List<int>> ints) => new JsonResult(
+        public IActionResult Post(List<List<int>> ints) => new JsonResult(
             $"a [{ints.Count},{ints[0].Count}] was received and deserialized as List<List<int>>.");
 
         [HttpPut]
-        public async Task<IActionResult> Put(int [,] ints) => new JsonResult(
+        public IActionResult Put(int [,] ints) => new JsonResult(
             $"a [{ints.GetLength(0)},{ints.GetLength(1)}] was received and deserialized as int[,].");
     }
 }
